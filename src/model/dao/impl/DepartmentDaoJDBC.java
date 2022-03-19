64bd53db38bd8155c222996ae303e836a9af4aba
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-03-19 01:52:55 -0300

    Remove department

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -12,6 +12,7 @@ import java.util.Map;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -79,7 +80,7 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 			if (rows == 0) throw new DbException("Id not exist!");
 			
 		} catch (SQLException e) {
-			throw new DbException(e.getMessage());
+			throw new DbIntegrityException(e.getMessage());
 		} finally {
 			DB.closeStatement(st);
 		}
